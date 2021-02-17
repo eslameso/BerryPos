1d47891a577864c0c0a63cc14854570f5e3db8af
Eslam Elsayed <engeso8@gmail.com>
2021-02-18 01:23:02 +0200

    Login Post

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Pos.Models;
@@ -17,7 +18,7 @@ namespace Pos.Controllers
         {
             _logger = logger;
         }
-
+         [Authorize]
         public IActionResult Index()
         {
             return View();

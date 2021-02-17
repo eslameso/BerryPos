401ea171f6e485f1b7e06cae47c06f63b57bc7ff
Eslam Elsayed <engeso8@gmail.com>
2021-02-17 23:04:10 +0200

    Register Post

--- Controllers/AccountControllers.cs
+++ Controllers/AccountControllers.cs
@@ -1,15 +1,56 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Pos.Models;
+using Pos.ViewModels;
 
 namespace Pos.Controllers
 {
-    public class AccountController :Controller
+    public class AccountController : Controller
     {
-        
-      [HttpGet]
-      public IActionResult Register(){
+        public UserManager<ApplicationUsers> UserManager { get; }
+        public SignInManager<ApplicationUsers> SignInManager { get; }
 
-        return View();
-      }
+        public AccountController(UserManager<ApplicationUsers> userManager, SignInManager<ApplicationUsers> signInManager)
+        {
+            this.SignInManager = signInManager;
+            this.UserManager = userManager;
+
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterMv model){
+         if (ModelState.IsValid)
+         {
+           var User =new ApplicationUsers{
+               UserName=model.UserName,
+               Email=model.Email
+           };
+           var Result= await UserManager.CreateAsync(User,model.Password);
+             if(Result.Succeeded)
+             {
+               await SignInManager.SignInAsync(User,isPersistent:false);
+               return RedirectToAction("Index","Home");
+             }
+             else
+             {
+               foreach (var item in Result.Errors)
+               {
+                   ModelState.AddModelError("",item.Description);
+               }
+
+             }
+         }
+
+         return View();
+        }
 
     }
 }

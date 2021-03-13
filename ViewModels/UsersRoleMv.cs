c525e97ee57a2fb2ab772338b9cbaaa9fa7b6a39
Eslam Elsayed <engeso8@gmail.com>
2021-03-13 23:38:39 +0200

    RolesFinish

---
+++ ViewModels/UsersRoleMv.cs
@@ -0,0 +1,13 @@
+namespace Pos.ViewModels
+{
+ public class UsersRoleMv
+ {
+   public string UserId { get; set; }
+   public string UserName { get; set; }
+   public bool IsSelected { get; set; }
+  
+
+ }
+
+
+}

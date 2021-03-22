7e5f853cd775adf8cf159d852bf24f93b47deefe
sheroukashraf <thequeen.shoshy@gmail.com>
2021-03-22 18:37:44 +0200

    Db

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -18,4 +18,8 @@ $(document).ready(function(){
             }
         }
     });
+    $(".nav-item").click(function () {
+        $(".nav-item").removeClass("active");
+        $(this).addClass("active");   
+    });
 });
